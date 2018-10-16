ba1af185728391deca7694a799982c16674d5c88
xuguanghai <15268131382@163.com>
2018-10-16 14:46:00 +0800

    12

--- designpattern/src/com/geekbetter/designpattern/construction/adapter/AdapaterClient.java
+++ designpattern/src/com/geekbetter/designpattern/construction/adapter/AdapaterClient.java
@@ -2,6 +2,7 @@ package com.geekbetter.designpattern.construction.adapter;
 
 public class AdapaterClient {
     public static void main(String[] args) {
+
         Target target;
         target = new Adapter();
         target.request();
